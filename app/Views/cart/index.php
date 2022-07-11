610c0569622d1bdb9a1725a9732962bd53b7961f
Boreneoux <ichlasul.ap@gmail.com>
2022-07-11 17:22:06 +0700

    End of projects(?)

--- app/Views/cart/index.php
+++ app/Views/cart/index.php
@@ -23,49 +23,66 @@
     </div>
 <?php endif; ?>
 
-<section id="cart" class="bg-[#f3f3f3] w-full mt-5">
+<section id="cart" class="bg-[#f3f3f3] w-full mt-5 pb-96">
     <div class="flex flex-col">
-        <h1 class="text-center mx-auto text-2xl pt-6">YOUR CART</h1>
+        <h1 class="text-center mx-auto text-3xl pt-8">YOUR CART</h1>
         <!-- Edit Cart -->
         <?php if (session()->get('cart_edit') == false) : ?>
             <?php $edit = "";
             $editTxtNum = "readonly='readonly'" ?>
-            <a href="<?= base_url('/cart/edit'); ?>" class="self-end mr-4 bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow">Edit Cart</a>
+            <a href="<?= base_url('/cart/edit'); ?>" class="self-end bg-white hover:bg-gray-100 text-gray-800 mr-6 md:mr-12 py-2 px-4 border border-gray-400 rounded shadow mb-2">Edit Cart</a>
         <?php else : ?>
             <?php $edit = "disabled='disabled'";
             $editTxtNum = ''; ?>
             <!-- <a href="<?php // echo base_url('/cart/save'); 
                             ?>">Save</a> -->
-            <a href="<?= base_url('/cart/cancel'); ?>">Cancel</a>
+            <a href="<?= base_url('/cart/cancel'); ?>" class="self-end bg-white hover:bg-gray-100 text-gray-800 mr-12 py-2 px-4 border border-gray-400 rounded shadow mb-6">Cancel</a>
         <?php endif; ?>
         <!-- End of Edit Cart -->
 
         <!-- Products -->
-        <div>
+        <div class="">
             <?php if (session()->get('cart_edit') == true) : $destination = 'save'; ?>
                 <form action="/cart/save" method="post">
                 <?php else : $destination = 'checkout'; ?>
                     <form action="/order" method="post">
                     <?php endif; ?>
                     <?php foreach ($cartItem as $key => $item) : ?>
-                        <div class="flex flex-row">
+                        <div class="flex flex-col mt-5">
                             <?php if ($destination == 'save') : ?>
                                 <input type="hidden" name="qtyold_<?= $key; ?>" value="<?= (int)$item['qty']; ?>">
                             <?php endif; ?>
-                            <input type="checkbox" name="key_<?= $key; ?>" id="key" <?= $edit; ?> value="<?= (int)$item['qty']; ?>">
-                            <img src="/img/Product/<?= $item['product_img']; ?>" alt="<?= $item['product_name']; ?>" class="bg-[#D1BEB0] group group-hover:bg-black transition-all ease-in-out w-[208px] h-[220px] mx-2 block mb-4 rounded-xl">
-                            <div>
-                                <label for="key"><?= $item['product_name']; ?></label>
-                                <input type="number" name="qty_<?= $key; ?>" id="qty_<?= $key; ?>" value="<?= (int)$item['qty']; ?>" <?= $editTxtNum; ?>>
-                                Size : <?= $item['size']; ?>| Price: IDR <?= number_format($item['price'], 0, ',', '.'); ?>| Total : IDR <?= number_format($item['qty_times_price'], 0, ',', '.'); ?>
-                                <a href="<?= base_url("cart/delete/$key"); ?>">Delete</a>
+                            <div class="flex flex-row mx-2 md:ml-10">
+                                <input type="checkbox" name="key_<?= $key; ?>" id="key" <?= $edit; ?> value="<?= (int)$item['qty']; ?>" class="self-start w-5 h-5 my-auto md:mx-8">
+                                <img src="/img/Product/<?= $item['product_img']; ?>" alt="<?= $item['product_name']; ?>" class="bg-[#D1BEB0]  w-[138px] h-[145px] mx-2 mb-4 rounded-xl">
+                                <div class="my-auto w-full">
+                                    <div class="grid grid-cols-2 gap-4 ">
+                                        <label for="key" class="text-xl font-bold col-span-1"><?= $item['product_name']; ?></label>
+                                        <p class="font-light text-sm md:justify-self-end md:pr-12">Price: IDR <?= number_format($item['price'], 0, ',', '.'); ?></p>
+                                    </div>
+                                    <h4>Size : <?= $item['size']; ?></h4>
+                                    <div class="grid grid-cols-2">
+                                        <div class="col-span-1">
+                                            <label for="qty_<?= $key; ?>">Quantity : </label>
+                                            <input type="number" name="qty_<?= $key; ?>" id="qty_<?= $key; ?>" value="<?= (int)$item['qty']; ?>" class="w-8 text-center border border-gray-400 rounded-md" <?= $editTxtNum; ?>>
+                                        </div>
+
+                                        <h2 class="col-span-1 mb-5 md:justify-self-end md:pr-12"> Total : <span class="font-bold"> IDR <?= number_format($item['qty_times_price'], 0, ',', '.'); ?> </span></h2>
+                                        <a href="<?= base_url("cart/delete/$key"); ?>" class="col-end-3 justify-self-end md:justify-self-end md:pr-12"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg></a>
+
+                                    </div>
+
+                                </div>
                             </div>
                         <?php endforeach; ?>
-                        <button type="submit">
+                        <button type="submit" class="text-gray-900 bg-[#D1BEB0] focus:ring-4 focus:outline-none  font-medium rounded-lg text-lg px-5 py-2.5 text-center mb-2 w-[150px] h-[50px] mx-auto hover:bg-opacity-80 mt-12">
                             <?php if ($destination == 'save') : ?>
                                 <?= $destination; ?>
-                            <?php else : echo $destination;
+                            <?php else : echo strtoupper($destination);
                             endif; ?>
+                            →
                         </button>
                     </form>
         </div>
--- app/Views/order/index.php
+++ app/Views/order/index.php
@@ -3,36 +3,81 @@
 
 <?= $this->section('content'); ?>
 <!-- disini nempatin isi atau content nya -->
-<form action="/order/buy"method='POST'>
-    <?php if(isset($validation)): ?>
-    <ul>
-        <?= $validation->listErrors(); ?>
-    </ul>
+
+<div class="w-full bg-[#f3f3f3] mb-8 pt-12 md:flex md:flex-col">
+    <h1 class="text-4xl text-center mb-10">CHECKOUT</h1>
+
+    <form action="/order/buy" method='POST'>
+        <?php if (isset($validation)) : ?>
+            <ul>
+                <?= $validation->listErrors(); ?>
+            </ul>
         <?php endif; ?>
-    <ul>
-    <?php foreach ($orderProceed as $index=>$order) : ?>
-        <input type="hidden" name="key_<?= $order; ?>" id="key_<?= $order; ?>" value="<?= $order; ?>">
-        <div>
-            <h2><?= $purchased[$index]['product_name']; ?></h2>
-           <li>qty: <?= $purchased[$index]['qty']; ?></li>
-           <li>total: IDR <?=number_format($purchased[$index]['qty_times_price'],0,',','.') ; ?></li>
+        <div class="flex flex-wrap">
+
+            <div class="px-8 mb-10 w-full grow md:w-1/2">
+                <?php foreach ($orderProceed as $index => $order) : ?>
+                    <div class="px-2 grid grid-cols-3 py-2 gap-4 md:gap-24">
+                        <input type="hidden" name="key_<?= $order; ?>" id="key_<?= $order; ?>" value="<?= $order; ?>">
+                        <?= $purchased[$index]['product_name']; ?>
+                        <p class="font-light">
+                            x<?= $purchased[$index]['qty']; ?>
+                        </p>
+                        Total : IDR <?= number_format($purchased[$index]['qty_times_price'], 0, ',', '.'); ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
+            <div class="w-full md:w-1/2">
+                <div class="md:ml-auto md:mr-10 mx-auto max-w-md max-h-lg bg-white mb-8 rounded-md px-4 py-8">
+                    <h2 class="text-black font-bold text-xl uppercase pb-4">Total Payment</h2>
+                    <div class="pb-4 grid grid-cols-2">
+                        <p class="font-light">Sub total product </p>
+                        <p class="font-light">IDR <?= number_format($total, 0, ',', '.'); ?></p>
+                        <p class="font-light">Delivery fee </p>
+                        <p class="border-black border-b-2 py-1 font-light">IDR <?= number_format(50000, 0, ',', '.'); ?></p>
+                        <p class="font-bold">Total </p>
+                        <p class="font-bold">IDR <?= number_format($total += 50000, 0, ',', '.'); ?></p>
+                    </div>
+
+                    <h1 class="text-black font-bold text-xl uppercase">Payments method</h1>
+                    <div class="pb-8 flex flex-wrap">
+                        <?php foreach ($payment as $pym) : ?>
+                            <div class="flex grow-0">
+                                <input type="radio" name="payment" id="<?= $pym['payment_id']; ?>" value="<?= $pym['payment_id']; ?>" class="mx-1">
+                                <label for="<?= $pym['payment_id']; ?>"><img src="/img/Payment/<?= $pym['payment_name']; ?>.png" alt="payment"></label>
+
+                            </div>
+                        <?php endforeach ?>
+                    </div>
+                    <div class="border bg-[#D1BEB0] text-center rounded hover:border-black focus:outline-none focus:ring-black active:border-black hover:bg-slate-300 text-xl">
+                        <button type="submit" name="submit" class="uppercase">Buy</button>
+                    </div>
+                </div>
+            </div>
+    </form>
+</div>
+</div>
+
+
+<!-- Newsletter start -->
+<section id="newsletter">
+    <div class=" bg-[#f3f3f3] w-full justify-center flex py-12 mb-8">
+        <div class="text-dark font-normal">
+            <h1 class="text-2xl">SUBSCRIBE TO OUR NEWSLETTER</h1>
+            <h3 class="text-center text-base mb-4">To get our latest promo and more cool stuf</h3>
+            <form action="" class="flex mb-4">
+                <input type="email" placeholder="Email" class="px-3 py-2 border border-black shadow block text-sm w-full placeholder:text-slate-400 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:border-sky-500 invalid:text-pink-700 invalid:focus:ring-pink-700 invalid:focus:border-pink-700">
+                <button class="border border-solid border-black items-center border-l-0 px-1"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="30" height="30" viewBox="0 0 256 256" xml:space="preserve">
+                        <g transform="translate(128 128) scale(0.72 0.72)">
+                            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(-175.05 -175.05000000000004) scale(3.89 3.89)">
+                                <polygon points="0,14.69 0,39.65 51,45 0,50.35 0,75.31 90,45 " style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) " />
+                            </g>
+                        </g>
+                    </svg></button>
+            </form>
         </div>
-    <?php endforeach; ?>
-    <h2>Total Payment</h2>
-    <div>
-        <ul>
-            <li>Sub total product : IDR <?= number_format($total,0,',','.'); ?></li>
-            <li>Delivery fee : IDR <?= number_format(50000,0,',','.'); ?></li>
-            <li>Total : <?= number_format($total+=50000,0,',','.'); ?></li>
-        </ul>
     </div>
-    </ul>
-    <?php foreach ($payment as $pym) : ?>
-        <div>
-            <label for="payment"><?= $pym['payment_name']; ?></label>
-            <input type="radio" name="payment" id="payment" value="<?= $pym['payment_id']; ?>" >
-        </div>
-    <?php endforeach ?>
-    <button type="submit" name="submit" >Buy</button>
-</form>
+</section>
+<!-- Newsletter end -->
 <?= $this->endSection(); ?>
